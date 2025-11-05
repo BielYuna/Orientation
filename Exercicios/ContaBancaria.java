8c4a32bdb3b46dc0bd797b52e3be846782fe3f1c
BielYuna <BielYuna@gmail.com>
2025-11-05 15:34:19 -0300

    Atributos, metodos, e contrutor, criados!

--- Exercicios/ContaBancaria.java
+++ Exercicios/ContaBancaria.java
@@ -1,10 +1,35 @@
 public class ContaBancaria{
+    /**Atributos Privados
+     *Usa do encapsulamento para proteger os atributos desta classe **/
     private String numeroConta;
     private double saldo;
     private String titular;
 
-    public void depositar(int saldo){
+      /**Construtor
+     *Define como os objetos dessa classe serão criados **/
+    public ContaBancaria(){
+    }
+
+    public ContaBancaria(String numeroConta, String titular, double saldo){
+        this.numeroConta = numeroConta;
+        this.titular = titular;
         this.saldo = saldo;
+    }
+    
+    /**Metodos Publicos
+     *Metodos são os serviços que serão executados **/
+    public void depositar(int saldo){
+        try{
+            if (saldo > 0){
+                this.saldo = saldo + this.saldo;
+                System.out.println("Deposito realizado com sucesso!"+"\nSaldo atual= "+(this.saldo + saldo));
+                return;
+            }else{
+                System.out.println("Valor do deposito deve ser maior que zero!");
+            }
+        }catch(Exception erro){
+            System.out.println("Erro ao depositar: "+erro.getMessage());
+        }
     }
     public void sacar(int saque){
         if (saldo >= saque){
@@ -14,7 +39,35 @@ public class ContaBancaria{
             System.out.println("Saldo insufuciente!");
         }
     }
-    public void dadosConta(){
-        System.out.println("Titular: "+titular+"\nNumero da conta: "+numeroConta+"\nSaldo: "+saldo);
+
+    public void consultarSaldo(){
+        System.out.println("Saldo atual= "+saldo);
     }
+
+    public void alterarTitular(String titular){
+        try{
+        if(titular.length() > 0){
+            System.out.println("Titular alterado com sucesso de "+this.titular+" para "+titular);
+            this.titular = titular;
+        }else{
+            System.out.println( "Titular nao pode ser vazio!" );
+            }
+        }catch(Exception erro){
+            System.out.println("Erro ao alterar titular: "+erro.getMessage());
+        }
+    }
+
+    public void dadosConta(){
+        System.out.println("DADOS CLIENTE"+"\nTitular: "+titular+"\nNumero da conta: "+numeroConta+"\nSaldo: "+saldo);
+    }
+
+    /**Metodos Getters e Setters
+     *Define os metodos para requisitar e adicionar valores aos atributos **/
+    public String getTitular(){
+        return titular;
+    }
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
 }
--- Exercicios/UsarConta.java
+++ Exercicios/UsarConta.java
@@ -1,8 +1,13 @@
 public class UsarConta {
     public static void main(String[] args) {
-        ContaBancaria servico = new ContaBancaria();
+        ContaBancaria servico = new ContaBancaria("12345-6","Gabriel",1000);
 
-        servico.depositar(50);
-        servico.sacar(3);
+        servico.dadosConta();
+        servico.depositar(500);
+        servico.sacar(2000);
+        servico.sacar(300);
+        servico.alterarTitular("");
+        servico.consultarSaldo();
+        servico.dadosConta();
     }
 }
